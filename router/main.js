e2ce82b8d19a34916bd02a4395d1df6e47a27c55
Anusekhar <anus@qburst.com>
2015-09-29 19:27:53 +0530

    database connection established

--- router/main.js
+++ router/main.js
@@ -1,9 +1,19 @@
 module.exports = function(app)
 {
      app.get('/',function(req,res){
-        res.render('index.html')
+        res.render('index.html');
+
+     });
+     app.post('/', function (req, res) {
+       res.render('home.html');
+
+     });
+     app.get('/home',function(req,res){
+        res.render('home.html');
+
      });
      app.get('/signup',function(req,res){
         res.render('signup.html');
     });
+    
 }
--- server.js
+++ server.js
@@ -1,14 +1,13 @@
 var express    =    require('express');
 var app        =    express();
 var mysql      = require('mysql');
-
-var connection = mysql.createConnection({
+var client = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
   password : '123',
   database : 'gmail'
 });
-connection.connect(function(err){
+client.connect(function(err){
 if(!err) {
     console.log("Database is connected ... \n\n");
 } else {
@@ -16,13 +15,6 @@ if(!err) {
 }
 });
 
-connection.query('SELECT * from details', function(err, rows) {
-connection.end();
-  if (!err)
-    console.log('The solution is: ', rows);
-  else
-    console.log('Error while performing Query.');
-  });
 
 require('./router/main')(app);
 app.set('views',__dirname + '/views');
@@ -31,4 +23,46 @@ app.engine('html', require('ejs').renderFile);
 
 var server     =    app.listen(3000,function(){
 console.log("Express is running on port 3000");
+
+});
+
+
+app.get('/process_get', function (req, res) {
+// Prepare output in JSON format
+response = {
+   user_name:req.query.username,
+   password:req.query.password
+ };
+ console.log(response);
+ client.query('SELECT * from details where name="'+response.user_name+'" and password="'+response.password+'" ', function(err, rows, fields) {
+ //client.end();
+   if (rows.length!=0)
+   {
+     console.log('loged in: ', rows);
+     res.render('home.html',{title: response.user_name });
+   }
+  else
+     {
+       console.log('Error while performing Query.');
+       res.render('index.html');
+     }
+
+   });
+
+});
+
+
+app.get('/process_post', function (req, res) {
+
+  client.query('INSERT INTO details ( name,password ) VALUES( "'+req.query.username+'", "'+req.query.password+'")',  function(err, rows, fields) {
+    if(err){
+      console.log('Error');
+    }
+    else {
+      console.log(req.query.username);
+      console.log('signup');
+    }
+
+    res.render('index.html');
+});
 });
